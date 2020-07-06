d349e818265b128ae6c624f1798b148612286a69
zara@bleusafe.com <zara@bleusafe.com>
2020-07-05 20:00:18 -0700

    Initial commit

---
+++ first/cmake-build-debug/CMakeFiles/first.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first.dir/main.cpp.o"
+  "first"
+  "first.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ first/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    // std::cout<<"Hello, World!"<<std::endl;
+    for (int i = 0; i< 5; i++ ){
+        std::cout << "the number is " << i << std::endl;
+    }
+    return 0;
+}
